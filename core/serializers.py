723ec1a1860b6fe2d7a9eb1b15d931aa64c4dfb2
Oksana <rosa111@mail.ru>
2022-12-04 12:32:07 +0700

    feat: user's profile

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
 
 from core.models import User
 
@@ -38,8 +40,19 @@ class LoginSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'password']
 
+    def create(self, validated_data):
+        user = authenticate(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        if not user:
+            raise AuthenticationFailed()
+        return user
+
 
 class ProfileSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email']
 
 
--- core/views.py
+++ core/views.py
@@ -1,13 +1,12 @@
-from django.contrib.auth import login, authenticate
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 from rest_framework import status
-from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.generics import CreateAPIView, GenericAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
 from core.models import User
-from core.serializers import SignUpSerializer, LoginSerializer
+from core.serializers import SignUpSerializer, LoginSerializer, ProfileSerializer
 
 
 class SignUpView(CreateAPIView):
@@ -15,29 +14,31 @@ class SignUpView(CreateAPIView):
     serializer_class = SignUpSerializer
 
 
+# @method_decorator(csrf_exempt, name='dispatch')
 class LoginView(GenericAPIView):
     queryset = User.objects.all()
     serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = authenticate(
-            request=request,
-            username=request.data.get('username'),
-            password=request.data.get('password')
-        )
-        if user is None:
-            raise AuthenticationFailed
-        else:
-            login(request, user)
-            return Response(serializer.data), status.HTTP_201_CREATED
+        user = serializer.save()
+        login(request, user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 # @ensure_csrf_cookie
-# class ProfileView(RetrieveUpdateDestroyAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = ProfileSerializer
-#     permission_classes = [IsAuthenticated|ReadOnly]
+class ProfileView(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
 
+    @ensure_csrf_cookie
+    def get_object(self):
+        return self.request.user
 
+    @ensure_csrf_cookie
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
