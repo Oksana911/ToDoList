6fe2eeae3d2cb08e6c4324e69fa3bd6ad4d05104
Oksana <rosa111@mail.ru>
2022-12-06 11:23:54 +0700

    fix urls

--- goals/urls.py
+++ goals/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from goals.views import GoalCategoryCreateView, GoalCategoryView
+from goals.views import GoalCategoryCreateView, GoalCategoryView, GoalCategoryListView
 
 urlpatterns = [
     path('goal_category/create', GoalCategoryCreateView.as_view(), name='cat_create'),
-    path('goal_category/list', GoalCategoryCreateView.as_view(), name='cat_list'),
+    path('goal_category/list', GoalCategoryListView.as_view(), name='cat_list'),
     path('goal_category/<pk>', GoalCategoryView.as_view(), name='cat'),
 ]
 
